981918d5063ea774f7dbed812f634eb0256aa4fd
lancegin <gin.lance.inside@hotmail.com>
2018-06-08 21:28:24 +0800

    add: constant httpMethod and resCode object

---
+++ src/renderer/utils/http-method.js
@@ -0,0 +1,11 @@
+// define all http methods
+const httpMethod = [
+  { command: 'get', name: 'GET' },
+  { command: 'post', name: 'POST' },
+  { command: 'put', name: 'PUT' },
+  { command: 'patch', name: 'PATCH' },
+  { command: 'delete', name: 'DELETE' },
+  { command: 'options', name: 'OPTIONS' },
+];
+
+export default httpMethod;
