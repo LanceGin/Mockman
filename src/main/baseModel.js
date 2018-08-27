f76f80493c4c175a0679e87bb065fa721079c485
lancegin <gin.lance.inside@hotmail.com>
2018-08-27 14:45:19 +0800

    upd: sequelize multiple query

--- src/main/baseModel.js
+++ src/main/baseModel.js
@@ -8,6 +8,9 @@ import Sequelize from 'sequelize';
 const sqlLocation = path.join(__static, '/dev.sqlite');
 const sequelize = new Sequelize('main', null, null, {
   dialect: 'sqlite',
+  dialectOptions: {
+    multipleStatements: true,
+  },
   storage: sqlLocation,
 });
 
