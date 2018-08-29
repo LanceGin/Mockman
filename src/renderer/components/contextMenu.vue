a7111e679e3e8fa1c7c8ca87a488f640f1669691
Gin <gin.lance.inside@hotmail.com>
2018-08-29 13:48:46 +0800

    upd: contextmenu style

--- src/renderer/components/contextMenu.vue
+++ src/renderer/components/contextMenu.vue
@@ -126,19 +126,22 @@
 
 <style scoped>
   .context-menu {
-    background: #fafafa;
-    border: 1px solid #bdbdbd;
-    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
+    background: #202225;
+    border: 0;
+    border-radius: 5px;
     display: block;
     margin: 0;
     padding: 0;
     position: fixed;
-    width: 250px;
+    width: 150px;
     z-index: 99999;
+  }
+  .context-menu:focus {
+    outline: none;
   }
   .context-menu ul {
     list-style: none;
-    padding: 10px 0;
+    padding: 0;
     margin: 0;
     font-size: 12px;
     font-weight: 600;
@@ -147,9 +150,10 @@
     margin: 0;
     padding: 10px 35px;
     cursor: pointer;
+    color: #fff;
   }
   .context-menu ul li:hover {
-    background: #1e88e5;
-    color: #fafafa;
+    background: #33c6c5;
+    color: #fff;
   }
 </style>
