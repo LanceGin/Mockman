e05e2f28a6d4f41f4f6c1b4e17909b99a42dfa96
lancegin <gin.lance.inside@hotmail.com>
2018-09-02 11:29:49 +0800

    del: axios dependencies

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,14 +1,12 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import axios from 'axios';
 
 import App from './App';
 import router from './router';
 import store from './store';
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
-Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 
