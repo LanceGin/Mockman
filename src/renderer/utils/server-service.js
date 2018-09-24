d3b75147dfad8b24981c321ac0078aeb9e742022
lancegin <gin.lance.inside@hotmail.com>
2018-09-24 19:58:05 +0800

    add: serverService class

---
+++ src/renderer/utils/server-service.js
@@ -0,0 +1,21 @@
+// server method with dynamic configuration
+
+export default class serverService {
+  constructor(props) {
+    super(props);
+  
+    this.state = {};
+  }
+
+  // start server
+  start() {}
+
+  // stop server
+  stop() {}
+
+  // set cors
+  serCors() {}
+
+  // ser routes
+  serRoutes() {}
+}
