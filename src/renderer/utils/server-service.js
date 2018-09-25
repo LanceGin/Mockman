9ebe85845c317909f7f53e2b0b549d17305a7f2c
Gin <gin.lance.inside@hotmail.com>
2018-09-25 10:54:06 +0800

    add: cors-middleware to set cors

--- src/renderer/utils/server-service.js
+++ src/renderer/utils/server-service.js
@@ -1,5 +1,6 @@
 // server method with dynamic configuration
 const express = require('express')
+const cors = require('cors');
 
 const listenPort = (service, port) => {
   service.listen(port, (err, res) => {
@@ -7,9 +8,14 @@ const listenPort = (service, port) => {
   });
 };
 
-export const start = (config) => {
+const setCors = (service) => {
+  service.use(cors());
+}
+
+export default const start = (config) => {
   const service = express();
   const port = config.port;
 
   listenPort(service, port);
+  setCors(service);
 }
