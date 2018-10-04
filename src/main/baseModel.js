a0d3abbdffe3e8a9602307726556c6a74dd782e5
lancegin <gin.lance.inside@hotmail.com>
2018-10-04 11:57:08 +0800

    fix: replace db file with userData

--- src/main/baseModel.js
+++ src/main/baseModel.js
@@ -4,10 +4,34 @@
 
 import path from 'path';
 import Sequelize from 'sequelize';
+import { app } from 'electron' // eslint-disable-line
 
-const sqlLocation = process.env.NODE_ENV === 'development'
-  ? path.join(__static, '/dev.sqlite')
-  : path.join(__dirname, '/../../../mockman.sqlite');
+const sqlLocation = path.join(app.getPath('userData'), '/mockman.sqlite');
+const createMock = `
+  CREATE TABLE IF NOT EXISTS "mocks" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "port" TEXT NOT NULL,
+    "prefix" TEXT NOT NULL,
+    "created_at" datetime NOT NULL,
+    "updated_at" datetime NOT NULL
+  );
+`;
+const creatApi = `
+  CREATE TABLE IF NOT EXISTS "apis" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "mock_id" INTEGER NOT NULL,
+    "path" TEXT NOT NULL,
+    "method" TEXT NOT NULL,
+    "res_code" TEXT NOT NULL,
+    "latency" TEXT NOT NULL,
+    "request" json NOT NULL,
+    "response" json NOT NULL,
+    "created_at" datetime NOT NULL,
+    "updated_at" datetime NOT NULL
+  );
+`;
 
 const sequelize = new Sequelize('main', null, null, {
   dialect: 'sqlite',
@@ -15,6 +39,9 @@ const sequelize = new Sequelize('main', null, null, {
     multipleStatements: true,
   },
   storage: sqlLocation,
+});
+sequelize.query(createMock).then(() => {
+  sequelize.query(creatApi).then();
 });
 
 export default sequelize;
