1a1f352bfc1510cf859782ed80246a75c5199f6c
lancegin <gin.lance.inside@hotmail.com>
2018-10-04 17:09:13 +0800

    fix: no more extraResources in the packager

--- .electron-vue/build.config.js
+++ .electron-vue/build.config.js
@@ -12,8 +12,5 @@ module.exports = {
   ignore: /(^\/(src|test|\.[a-z]+|README|yarn|static|dist\/web))|\.gitkeep/,
   out: path.join(__dirname, '../build'),
   overwrite: true,
-  platform: process.env.BUILD_TARGET || 'all',
-  extraResource: [
-    path.join(__dirname, '../static/mockman.sqlite')
-  ]
+  platform: process.env.BUILD_TARGET || 'all'
 }
