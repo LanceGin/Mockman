11dadb601732fa6ea96f77607d8ab812f37b155e
Gin <gin.lance.inside@hotmail.com>
2018-11-14 18:18:33 +0800

    upd: import new icon

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import './assets/iconfont.css';
 import App from './App';
 import router from './router';
 
